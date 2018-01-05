b8a19529cc9589a72ae08df3b7bf114cd57debee
lian <530420449@qq.com>
2018-01-05 16:45:49 +0800

    新建了项目

---
+++ src/com/zhiyou100/test/Test.java
@@ -0,0 +1,16 @@
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: Test  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:48
+*  
+*/
+public class Test {
+
+	
+	public static void main(String[] args) {
+		System.out.println("测试项目");
+	}
+}
