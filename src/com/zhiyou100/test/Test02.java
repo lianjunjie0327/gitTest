65c85403b082399034b9e7bf927086a9bdf167c3
lian <530420449@qq.com>
2018-01-05 16:48:10 +0800

    新建文件

---
+++ src/com/zhiyou100/test/Test02.java
@@ -0,0 +1,16 @@
+package com.zhiyou100.test;
+
+/**  
+* @ClassName: Test02  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:45
+*  
+*/
+public class Test02 {
+ 
+	
+	public static void main(String[] args) {
+		System.out.println("新增文件");
+	}
+}
